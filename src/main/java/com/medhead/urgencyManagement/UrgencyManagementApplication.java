ef33f7411496c6359ed3933af32433bc39aab6a5
Clément <jour.clement@gmail.com>
2023-03-20 13:35:40 +0100

    setup project

---
+++ src/main/java/com/medhead/urgencyManagement/UrgencyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.urgencyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrgencyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrgencyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medhead/urgencyManagement/UrgencyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhead.urgencyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrgencyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
