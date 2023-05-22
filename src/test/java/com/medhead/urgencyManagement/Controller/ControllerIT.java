c113e5dd79b854deee17e86aa9c370879606b444
Clément <jour.clement@gmail.com>
2023-05-22 06:44:10 +0200

    test running only UTs
    WIP

--- src/test/java/com/medhead/urgencyManagement/Controller/ControllerIT.java
+++ src/test/java/com/medhead/urgencyManagement/Controller/ControllerIT.java
@@ -1,6 +1,6 @@
 package com.medhead.urgencyManagement.Controller;
 
-/*import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ControllerITests {
+public class ControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
@@ -32,5 +32,5 @@ public class ControllerITests {
                 .andExpect(jsonPath("$.id", is(33)));
     }
 
-}*/
+}
 
