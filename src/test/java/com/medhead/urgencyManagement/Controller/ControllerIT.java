e3a0025218d6140890fab3d7cef62b475c0a5cfa
Clément <jour.clement@gmail.com>
2023-07-30 16:45:33 +0200

    udpating test after security addition

--- src/test/java/com/medhead/urgencyManagement/Controller/ControllerIT.java
+++ src/test/java/com/medhead/urgencyManagement/Controller/ControllerIT.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.hamcrest.CoreMatchers.is;
@@ -18,6 +19,7 @@ public class ControllerIT {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     public void testGetHospital() throws Exception {
         String latitude = "51.585049";
         String longitude = "-0.175270";
