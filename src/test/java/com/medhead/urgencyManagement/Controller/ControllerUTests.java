55a7129165326376171a1f690ebb394361abbf70
Clément <jour.clement@gmail.com>
2023-07-30 17:25:26 +0200

    fixing test really for real

--- src/test/java/com/medhead/urgencyManagement/Controller/ControllerUTests.java
+++ src/test/java/com/medhead/urgencyManagement/Controller/ControllerUTests.java
@@ -1,10 +1,14 @@
 package com.medhead.urgencyManagement.Controller;
 
+import com.medhead.urgencyManagement.configuration.JsonWebTokenFilter;
 import com.medhead.urgencyManagement.controller.UrgencyController;
+import com.medhead.urgencyManagement.repository.AuthorizationRepository;
 import com.medhead.urgencyManagement.service.UrgencyService;
+import com.netflix.discovery.converters.Auto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -20,6 +24,9 @@ public class ControllerUTests {
 
     @MockBean
     private UrgencyService urgencyService;
+
+    @MockBean
+    AuthorizationRepository authorizationRepository;
 
     @Test
     @WithMockUser
